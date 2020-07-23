55471f16f314607e4aeccc7f44280880aa734956
Diego Resende <wdresende@gmail.com>
2020-07-23 16:38:06 -0300

    Create some Test Case for demonstration

--- pacients/tests.py
+++ pacients/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Pacient
+
+class PacientModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Pacient.objects.create(name="Diego", date_birth="1996-10-01", test_type="RT-PCR", result="P")
+    
+    def testName(self):
+        pacient = Pacient.objects.get(id=1)
+        name = pacient.name
+        self.assertEquals(name, 'Diego')
+
+    def testNameMaxLength(self):
+        pacient = Pacient.objects.get(id=1)
+        name_max_lenght = pacient._meta.get_field('name').max_length
+        self.assertEquals(name_max_lenght, 100)
