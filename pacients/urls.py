6d4080df8ee0562cb12c23c4bb54a629c91fb987
Diego Resende <wdresende@gmail.com>
2020-07-23 16:04:28 -0300

    update routes

--- pacients/urls.py
+++ pacients/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('pacients/', views.PacientList.as_view(), name='pacient_list'),
-    path('pacients/create', views.PacientAdd.as_view(), name='pacient_create'),
-    path('pacients/update/<int:id>', views.PacientUpdate.as_view(), name='pacient_update'),
+    path('', views.PacientList.as_view(), name='pacient_list'),
+    path('create', views.PacientAdd.as_view(), name='pacient_create'),
+    path('update/<int:id>', views.PacientUpdate.as_view(), name='pacient_update'),
 ]
